3f8abb53686089b678e9ba4709acdbe619786bba
Robert Soule <robert.soule@barefootnetworks.com>
2017-10-24 07:29:07 -0700

    Don't checkout SIGCOMM branch. (#62)
    * Created new subdirectory for Fall developer day.
    
    * Created vm directory for P4D2_2017_Fall. Fixed emacs permission problem.
    
    * Remove checkout of SIGCOMM branch.

--- P4D2_2017_Fall/vm/user-bootstrap.sh
+++ P4D2_2017_Fall/vm/user-bootstrap.sh
@@ -38,7 +38,6 @@ cd ..
 pip install crcmod
 git clone https://github.com/p4lang/tutorials
 cd tutorials
-git checkout sigcomm_17
 cd ..
 sudo mv tutorials /home/p4
 sudo chown -R p4:p4 /home/p4/tutorials
