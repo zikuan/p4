f827e874fbb56a1fd40c4fdf8679313bb3c8d5de
Robert MacDavid <robertmacdavid@gmail.com>
2017-10-24 18:27:34 -0400

    Added vim syntax highlighting to provisioning script (#63)
    * fixed provision bug, added vim syntax
    
    * reverted my 'bug fix'

--- P4D2_2017_Fall/vm/user-bootstrap.sh
+++ P4D2_2017_Fall/vm/user-bootstrap.sh
@@ -37,8 +37,6 @@ cd ..
 # Tutorials
 pip install crcmod
 git clone https://github.com/p4lang/tutorials
-cd tutorials
-cd ..
 sudo mv tutorials /home/p4
 sudo chown -R p4:p4 /home/p4/tutorials
 
@@ -46,4 +44,16 @@ sudo chown -R p4:p4 /home/p4/tutorials
 sudo cp p4_16-mode.el /usr/share/emacs/site-lisp/
 echo "(add-to-list 'auto-mode-alist '(\"\\.p4\\'\" . p4_16-mode))" | sudo tee /home/p4/.emacs
 sudo chown p4:p4 /home/p4/.emacs
+
+# Vim
+cd /home/vagrant
+mkdir .vim
+cd .vim
+mkdir ftdetect
+mkdir syntax
+echo "au BufRead,BufNewFile *.p4      set filetype=p4" >> ftdetect/p4.vim
+cp /home/vagrant/p4.vim syntax/p4.vim
+cd /home/vagrant
+sudo mv .vim /home/p4/.vim
+sudo chown -R p4:p4 /home/p4/.vim
 
